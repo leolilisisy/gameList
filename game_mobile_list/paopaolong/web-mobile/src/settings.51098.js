eeba545e9373cb9a6a0d08ada91b47598a2a9a1b
Joker@100 <li-l02@hotmail.com>
2024-08-25 18:14:04 +0800

    添加偷金子，修改泡泡龙

---
+++ game_mobile_list/paopaolong/web-mobile/src/settings.51098.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/ppl/splash.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"52884",main:"14e50"}};
